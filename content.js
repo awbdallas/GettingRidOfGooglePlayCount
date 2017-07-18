18ea52ae6eb9bd5e18672d5ecdba7402229840cd
Austin Briggs <Awbdallas@gmail.com>
2017-07-18 17:31:10 -0500

    Uhhh, so...this actually works better

--- content.js
+++ content.js
@@ -1,10 +1,3 @@
 var checkExistPlaycounter = setInterval(function() {
-
-	if($("#music-content > div.detail-view.album-view.same-artist.g-content.view-transition > div:nth-child(2) > div > table > thead > tr > th:nth-child(7)").length){
-		$("#music-content > div.detail-view.album-view.same-artist.g-content.view-transition > div:nth-child(2) > div > table > thead > tr > th:nth-child(7)").hide();
-	}
-
-	if($("#music-content > div.g-content.view-transition > div > div:nth-child(3) > div.songlist-container.material-shadow-z1 > table > thead > tr > th:nth-child(5)").length) {
-		$("#music-content > div.g-content.view-transition > div > div:nth-child(3) > div.songlist-container.material-shadow-z1 > table > thead > tr > th:nth-child(5)").hide();
-	}
-})
+	$("td[data-col=play-count]").hide();
+}, 100);
